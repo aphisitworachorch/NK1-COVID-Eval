b1e95e221cff42be4254b2c0b1f02863089b6b58
Asanan Aphisitworachorch <thaixperiasp@gmail.com>
2021-04-09 11:38:18 +0700

    update

---
+++ resources/js/Pages/COVID/IntroSelect.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <b-container class="py-2">
+            <b-container class="py-1">
+                <h3 style="text-align: center">CAUTION ! COVID-19 (UK-Strain) is Spreading</h3>
+            </b-container>
+            <b-container class="py-1">
+                <h3 style="text-align: center">Person who came from</h3>
+            </b-container>
+            <b-container class="py-1">
+                <h3 style="text-align: center">Bangkok / Nonthaburi / Samut Prakarn / Nakhon Pathom / Samut Songkhram / Pathum Thani / Tak / Ratchaburi</h3>
+            </b-container>
+        </b-container>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "IntroSelect"
+}
+</script>
+
+<style scoped>
+
+</style>
